b35c7c6ef3974c2026be6c4273c32b7979f7f520
Alex <reyesgx11@gmail.com>
2021-05-15 11:37:16 -0700

    cards added => format !!!

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,13 +1,45 @@
 import '../App.css';
 import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+
+const projects = [
+    {
+        title: 'My Portfolio ',
+        text: 'Web app build with React',
+        link: 'github.com/iamalx',
+    },
+    {
+        title: 'My Portfolio ',
+        text: 'Web app build with React',
+        link: 'github.com/iamalx',
+    },
+    {
+        title: 'My Portfolio ',
+        text: 'Web app build with React',
+        link: 'github.com/iamalx',
+    }
+]
 
 const Portfolio = () => {
 
     const [ userList, setUserList ] = useState([])
 
-    
+
     return (
         <div className='error-bar viewport-hg' >
+            {console.log(projects)}
+            {projects.map(elem => (
+                    <Card key={elem.title} style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>{elem.title}</Card.Title>
+                                <Card.Text>
+                                {elem.text}
+                                </Card.Text>
+                            <Card.Link href="#">{elem.link}</Card.Link>
+                        </Card.Body>
+                    </Card>
+                ))
+            }
             
         </div>
     );
